c3cca530080393cfa00c01796b9711d470e946b1
Fukulyn <a098838628@gmail.com>
2024-12-13 10:28:32 +0800

    Update

--- src/enum/api.ts
+++ src/enum/api.ts
@@ -1,4 +1,5 @@
 export enum api{
-    findAll = "http://localhost:5175/api/v1/user/findAll",
-    deleteById = "http://localhost:5175/api/v1/user/deleteByID"
+    findAll = "http://127.0.0.1:27107/api/v1/user/findAll",
+    deleteById = "http://127.0.0.1:27107/api/v1/user/deleteByID",
+    updateNameByID = "http://127.0.0.1:27107/api/v1/user/updateNameByID"
 }
